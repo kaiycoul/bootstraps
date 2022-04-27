e9cc31494f84b984a22087967bc4be7404fbbc5a
kaiycoul <88100458+kaiycoul@users.noreply.github.com>
2022-04-27 19:13:16 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+input.onPinReleased(TouchPin.P0, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+radio.onReceivedValue(function (name, value) {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+loops.everyInterval(500, function () {
+	
+})
+basic.forever(function () {
+	
+})
